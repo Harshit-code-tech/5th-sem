dc44a90f9fa21d514f12fc7e6aa2a2f9bb1a871a
Harshit Ghosh <harshitghosh6@gmail.com>
2024-09-05 08:14:14 +0530

    modifications

--- 5th sem/CN/CN/LAB2/assignment/ass2/client.c
+++ 5th sem/CN/CN/LAB2/assignment/ass2/client.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024  // Define buffer size for messages
 
 int main(int argc, char *argv[]) {
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in client_addr = { .sin_family = AF_INET, .sin_port = htons(atoi(argv[2])), .sin_addr.s_addr = inet_addr(argv[1]) };
-    struct sockaddr_in server_addr = { .sin_family = AF_INET, .sin_port = htons(atoi(argv[4])), .sin_addr.s_addr = inet_addr(argv[3]) };
-    char buffer[BUFFER_SIZE];
+    if (argc != 5) return 1;  // Ensure correct usage with command-line arguments
 
-    bind(client_socket, (struct sockaddr*)&client_addr, sizeof(client_addr));
-    connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    char *client_ip = argv[1];  // Extract client IP from arguments
+    int client_port = atoi(argv[2]);  // Extract client port from arguments
+    char *server_ip = argv[3];  // Extract server IP from arguments
+    int server_port = atoi(argv[4]);  // Extract server port from arguments
 
-    fgets(buffer, BUFFER_SIZE, stdin);
-    buffer[strcspn(buffer, "\n")] = 0;
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);  // Create a TCP socket
 
-    send(client_socket, buffer, strlen(buffer), 0);
+    struct sockaddr_in client_addr = {AF_INET, htons(client_port), inet_addr(client_ip)}, 
+                       server_addr = {AF_INET, htons(server_port), inet_addr(server_ip)};
+    
+    bind(client_fd, (struct sockaddr*)&client_addr, sizeof(client_addr));  // Bind client to IP and port
+    connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));  // Connect to the server
 
-    recv(client_socket, buffer, BUFFER_SIZE, 0);
-    printf("Received: \"%s\"\n", buffer);
+    socklen_t server_len = sizeof(server_addr);  // Get server info
+    getpeername(client_fd, (struct sockaddr*)&server_addr, &server_len);
+    char server_conn_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &server_addr.sin_addr, server_conn_ip, INET_ADDRSTRLEN);
+    int server_conn_port = ntohs(server_addr.sin_port);
 
-    close(client_socket);
+    char buffer[BUFFER_SIZE];  // Buffer to hold messages
+    printf("Enter message to send: ");
+    fgets(buffer, BUFFER_SIZE, stdin);  // Get input from the user
+    buffer[strcspn(buffer, "\n")] = '\0';  // Remove the newline character
+
+    send(client_fd, buffer, strlen(buffer), 0);  // Send the message
+    printf("Client IP: %s, Sent: \"%s\"\n", client_ip, buffer);
+
+    int bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);  // Receive the reversed message
+    buffer[bytes_received] = '\0';  // Null-terminate the received message
+
+    // Display the server's IP, port, and the received message
+    printf("Received from Server IP: %s, Port: %d, Message: \"%s\"\n", server_conn_ip, server_conn_port, buffer);
+
+    close(client_fd);  // Close the socket
 
     return 0;
 }
--- 5th sem/CN/CN/LAB2/assignment/ass2/server.c
+++ 5th sem/CN/CN/LAB2/assignment/ass2/server.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024  // Buffer size for messages
 
+// Function to reverse a string
 void reverse_string(char *str) {
-    for (int i = 0, n = strlen(str); i < n / 2; i++) {
-        char temp = str[i];
-        str[i] = str[n - i - 1];
-        str[n - i - 1] = temp;
+    for (int i = 0, len = strlen(str); i < len / 2; i++) {
+        char temp = str[i];               // Swap characters
+        str[i] = str[len - i - 1];        // from start and end
+        str[len - i - 1] = temp;
     }
 }
 
 int main(int argc, char *argv[]) {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0), client_socket;
-    struct sockaddr_in server_addr = { .sin_family = AF_INET, .sin_port = htons(atoi(argv[2])), .sin_addr.s_addr = inet_addr(argv[1]) };
-    char buffer[BUFFER_SIZE];
+    // Ensure correct usage of command-line arguments
+    if (argc != 3) return printf("Usage: %s <server_ip> <server_port>\n", argv[0]), 1;
 
-    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    listen(server_socket, 5);
+    // Create server socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0), client_fd;
+    
+    // Setup server address (IP, Port, and address family)
+    struct sockaddr_in server_addr = {AF_INET, htons(atoi(argv[2])), inet_addr(argv[1])}, client_addr;
+    
+    // Buffer to store received message and variable for client IP address
+    char buffer[BUFFER_SIZE], client_ip[INET_ADDRSTRLEN];
+    
+    // Variable to store size of client address
+    socklen_t client_len = sizeof(client_addr);
 
-    client_socket = accept(server_socket, NULL, NULL);
-    recv(client_socket, buffer, BUFFER_SIZE, 0);
+    // Bind the server to IP and port
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
+    // Listen for incoming connections
+    listen(server_fd, 1);
+    printf("Server listening on %s:%s\n", argv[1], argv[2]);
+
+    // Accept a connection from a client
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+
+    // Convert client IP from binary to text form
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+    // Receive message from the client
+    int bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
+    buffer[bytes_received] = '\0';  // Null-terminate the received string
+
+    // Display client's IP, port, and the received message
+    printf("Received from Client IP: %s, Port: %d, Message: \"%s\"\n", client_ip, ntohs(client_addr.sin_port), buffer);
+
+    // Reverse the received message
     reverse_string(buffer);
 
-    send(client_socket, buffer, strlen(buffer), 0);
+    // Send the reversed message back to the client
+    send(client_fd, buffer, strlen(buffer), 0);
+    printf("Server IP: %s, Sent: \"%s\"\n", argv[1], buffer);
 
-    close(client_socket);
-    close(server_socket);
+    // Close client and server sockets
+    close(client_fd);
+    close(server_fd);
 
     return 0;
 }
